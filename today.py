b2376929491dc846a8c444bf171a8bc2385e70f5
Winn <149982816+winnn25@users.noreply.github.com>
2026-07-12 17:32:06 +0800

    Update today.py

--- today.py
+++ today.py
@@ -349,6 +349,7 @@ def user_getter(username):
     }'''
     variables = {'login': username}
     request = simple_request(user_getter.__name__, query, variables)
+    print('DEBUG RESPONSE:', request.json())
     return {'id': request.json()['data']['user']['id']}, request.json()['data']['user']['createdAt']
 
 
